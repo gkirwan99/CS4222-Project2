ddb96a29a1c794ab6c9698910ecd10ceddf869b7
gkirwan99 <37332317+gkirwan99@users.noreply.github.com>
2018-04-25 23:00:15 +0100

    Update Facility.java

--- Facility.java
+++ Facility.java
@@ -5,7 +5,7 @@ public class Facilities {
 	private int pricePerHour ;
 	private Date decomissionedUntilDate ;
 	
-	facility(int aFacilityID, String aFacilityName, int aPricePerHour, date aDecomissionUntilDate) {
+	Facility(int aFacilityID, String aFacilityName, int aPricePerHour, LocalDate aDecomissionUntilDate) {
 		facilityID = aFacilityID ;
 		facilityName = aFacilityName ;
 		pricePerHour = aPricePerHour ;
@@ -16,15 +16,31 @@ public class Facilities {
 		return facilityID ;
 	}
 	
+	public void setFacilityID(int aFacilityID) {
+		facilityID = aFacilityID ;
+	}
+	
 	public getFacilityName() {
 		return facilityName ;
 	}
 	
-	public getPricePerHour() {
+	public void setFacilityName(String aFacilityName) {
+		facilityName = aFacilityName ;
+	}
+	
+	public String getPricePerHour() {
 		return pricePerHour ;
 	}
 	
-	public getDecommisionedUntilDate() {
+	public void setPricePerHour(aPricePerHour) {
+		pricePerHour = aPricePerHour ;
+	}
+	
+	public LocalDate getDecommisionedUntilDate() {
 		return decomissionedUntilDate ;
+	}
+	
+	public void setDecommisionedUntilDate(aDecomissionUntilDate) {
+		decomissionUntilDate = aDecomissionUntilDate ;
 	}
 }
